30509ee30907678e4b977e74dcf3866de54bc862
Alexander Yee <alexanderyee@email.arizona.edu>
2015-10-23 14:50:54 -0700

    second

--- src/view/NetpaintGUI.java
+++ src/view/NetpaintGUI.java
@@ -1,6 +1,7 @@
 package view;
 
 import java.awt.Graphics;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.swing.JFrame;
@@ -8,6 +9,10 @@ import javax.swing.JFrame;
 import model.PaintObject;
 // Alex Yee
 public class NetpaintGUI extends JFrame{
+	private List<PaintObject> objects;
+	public NetpaintGUI() {
+		objects = new ArrayList<PaintObject>();
+	}
     private void drawShapesOnMe(Graphics g, List<PaintObject> shapes) {
     	
     }
